0a24749677769a38ac65ab2f9fcb705268e6f4b1
sajib689 <jibonraja11@gmail.com>
2023-09-22 13:10:54 +0600

    add database for reviews

--- src/Components/Review/Review.jsx
+++ src/Components/Review/Review.jsx
@@ -1,14 +1,34 @@
 
 
 const Review = () => {
+    const handlePost = e => {
+        e.preventDefault()
+        const form = e.target
+        const name = form.name.value 
+        const imageURl = form.imageURl.value
+        const comment = form.comment.value
+        const reviews = {name,imageURl, comment}
+        fetch('http://localhost:3000/reviews',{
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(reviews)
+        })
+        .then( res => res.json())
+        .then(data => {
+            console.log(data)
+        })
+    }
     return (
        <div className="grid grid-cols-2 bg-base-200">
         
-         <div className="grid grid-cols">
-            <input type="text" placeholder="Type here name" className="mb-3 input input-bordered w-full max-w-xs" />
-            <textarea type="" placeholder="Type here message" className="mb-3 input input-bordered w-full max-w-xs" />
+         <form onSubmit={handlePost} className="grid grid-cols">
+            <input name="name" type="text" placeholder="Type here name" className="mb-3 input input-bordered w-full max-w-xs" />
+            <input name="imageURl" type="text" placeholder="Type here image URL" className="mb-3 input input-bordered w-full max-w-xs" />
+            <textarea name="comment" type="" placeholder="Type here message" className="mb-3 input input-bordered w-full max-w-xs" />
             <input className="btn max-w-xs text-white bg-[#0178BD] hover:bg-[#0487d3]" type="submit" value='Post'/>
-        </div>
+        </form>
         <div>review</div>
        </div>
     );
