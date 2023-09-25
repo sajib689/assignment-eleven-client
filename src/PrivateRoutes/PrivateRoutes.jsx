dd32eb1fbcf7b83b697dcfebcf873c9fb829c8cc
sajib689 <jibonraja11@gmail.com>
2023-09-25 16:03:44 +0600

    loader spiner added in private route

--- src/PrivateRoutes/PrivateRoutes.jsx
+++ src/PrivateRoutes/PrivateRoutes.jsx
@@ -3,24 +3,28 @@ import { AuthContext } from "../Providers/AuthProviders";
 import { Navigate } from "react-router-dom";
 import { Bars } from "react-loader-spinner";
 
-
-const PrivateRoutes = ({children}) => {
-    const {user,loading} = useContext(AuthContext)
-    if(loading){
-        return <Bars
-        height="80"
-        width="80"
-        color="#4fa94d"
-        ariaLabel="bars-loading"
-        wrapperStyle={{}}
-        wrapperClass=""
-        visible={true}
-      />
-    }
-   if(user) {
-    return children
-   }
-   return <Navigate to='/login'></Navigate>
+const PrivateRoutes = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center">
+        {" "}
+        <Bars
+          height="80"
+          width="80"
+          color="#4fa94d"
+          ariaLabel="bars-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+        />
+      </div>
+    );
+  }
+  if (user) {
+    return children;
+  }
+  return <Navigate to="/login"></Navigate>;
 };
 
 export default PrivateRoutes;
